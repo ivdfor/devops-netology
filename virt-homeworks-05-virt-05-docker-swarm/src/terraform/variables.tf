4e6b5782d3c7f486b379f2f09eb2a18ff64a045d
Igor Derevianko <ivdfor@gmail.com>
2022-08-15 13:08:51 -0700

    DZ 6-1 SUBD

---
+++ virt-homeworks-05-virt-05-docker-swarm/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gvkmq81kjk1dt8986e"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gvpgk1m2f46g0u3bpc"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8nirn8ouah72btb68g"
+}
